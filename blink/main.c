c652c852f5daeabcb6e4e2ccc80c2a8a8b758082
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-09-11 11:01:29 +0800

    Move bcm2835 locally

--- blink/main.c
+++ blink/main.c
@@ -1,25 +1,29 @@
 #include "util.h"
 
 int main() {
+  // counter
+  int i;
+  // both PINs mask
+  uint32_t pin_mask = mask_multi(PIN07, PIN16);
+  // both PINs high
+  uint32_t pin_high = mask_multi(PIN07, PIN16);
+  // both PINs low
+  uint32_t pin_low = 0;
+
   // initialise the library
-  if (!bcm2835_init()) return 1;
+  if (!bcm2835_init())
+    return 1;
 
   // set function of PIN07 to output
   bcm2835_gpio_fsel(PIN07, BCM2835_GPIO_FSEL_OUTP);
   // set function of PIN16 to output
   bcm2835_gpio_fsel(PIN16, BCM2835_GPIO_FSEL_OUTP);
 
-  // both PINs mask
-  uint32_t pn_mask = mask_multi(PIN07, PIN16);
-  // both PINs high
-  uint32_t pn_high = mask_multi(PIN07, PIN16);
-  // both PINs low
-  uint32_t pn_low = 0;
-
-  for (int i = 0; i < 50; ++i) {
-    bcm2835_gpio_write_mask(pn_high, pn_mask);
+  // loop
+  for (i = 0; i < 50; ++i) {
+    bcm2835_gpio_write_mask(pin_high, pin_mask);
     bcm2835_delay(50);
-    bcm2835_gpio_write_mask(pn_low, pn_mask);
+    bcm2835_gpio_write_mask(pin_low, pin_mask);
     bcm2835_delay(50);
   }
 
